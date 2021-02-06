7b961453634a6e664bf0c1a1cc4b47f01507ec5b
Kamaz <kamaz@xsimple.org>
2021-02-06 08:39:31 +0000

    feat: add success handler to cognito templates

--- packages/sls-aws/src/cognito/user-pool-handler.ts
+++ packages/sls-aws/src/cognito/user-pool-handler.ts
@@ -1,11 +1,18 @@
-import { EnvironmentConfig, Handler, SlsEnvironment, environment } from '@cabiri-io/sls-env'
+import { EnvironmentConfig, Handler, SlsEnvironment, SuccessParams, environment } from '@cabiri-io/sls-env'
 import type { Context, PreSignUpTriggerEvent, PreTokenGenerationTriggerEvent } from 'aws-lambda'
 
 type PreSignUpHandler = Handler<PreSignUpTriggerEvent, Context, Promise<PreSignUpTriggerEvent>>
 
+const responseOrError = <T>({ result }: SuccessParams<T>): T | never => {
+  if (result) {
+    return result
+  }
+  throw Error("expected event result to be present but it wasn't")
+}
+
 const cognitoUserPoolPreSignUp = <D, C = never>(
   config?: EnvironmentConfig<PreSignUpHandler>
-): SlsEnvironment<PreSignUpHandler, C, D> => environment<PreSignUpHandler, C, D>(config)
+): SlsEnvironment<PreSignUpHandler, C, D> => environment<PreSignUpHandler, C, D>(config).successHandler(responseOrError)
 
 type PreTokenGenerationHandler = Handler<
   PreTokenGenerationTriggerEvent,
@@ -15,7 +22,8 @@ type PreTokenGenerationHandler = Handler<
 
 const cognitoUserPoolPreTokenGeneration = <D, C = never>(
   config?: EnvironmentConfig<PreTokenGenerationHandler>
-): SlsEnvironment<PreTokenGenerationHandler, C, D> => environment<PreTokenGenerationHandler, C, D>(config)
+): SlsEnvironment<PreTokenGenerationHandler, C, D> =>
+  environment<PreTokenGenerationHandler, C, D>(config).successHandler(responseOrError)
 
 export type { PreSignUpHandler, PreTokenGenerationHandler }
 export { cognitoUserPoolPreSignUp, cognitoUserPoolPreTokenGeneration }
