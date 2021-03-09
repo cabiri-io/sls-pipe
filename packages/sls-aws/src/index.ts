49f0232038cb06176a6e295eebfaaa8d7ec6b7c7
Kamaz <kamaz@xsimple.org>
2021-03-09 09:59:36 +0000

    fix: export all api gateway functions

--- packages/sls-aws/src/index.ts
+++ packages/sls-aws/src/index.ts
@@ -1,4 +1,3 @@
 export { snsMessage, snsMessages } from './sns'
-export { apiGatewayV2, createSuccessResponse } from './api-gateway'
-export type { APIGatewayV2Handler } from './api-gateway'
+export * from './api-gateway'
 export * from './cognito'
