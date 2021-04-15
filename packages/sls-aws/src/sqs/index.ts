9c8c3a4438385290aacae2f75ee49e72609c4ec9
George Gunderson <georgegunderson@192-168-1-103.tpgi.com.au>
2021-04-15 11:13:27 +1000

    feat: sqs handler

---
+++ packages/sls-aws/src/sqs/index.ts
@@ -0,0 +1,17 @@
+import { EnvironmentConfig, Handler, SlsEnvironment, environment } from '@cabiri-io/sls-env'
+import type { Context, SQSEvent } from 'aws-lambda'
+import { response } from '../reponse/response-or-error'
+import { jsonSQSMessage } from './json-sqs-message'
+import { jsonSQSMessages } from './json-sqs-messages'
+
+export type SQSHandler = Handler<SQSEvent, Context, Promise<void>>
+
+export const sqsMessage = <D, P, C = never>(
+  config?: EnvironmentConfig<SQSHandler>
+): SlsEnvironment<SQSHandler, C, D, P> =>
+  environment<SQSHandler, C, D, P>(config).payload(jsonSQSMessage).successHandler(response)
+
+export const sqsMessages = <D, P, C = never>(
+  config?: EnvironmentConfig<SQSHandler>
+): SlsEnvironment<SQSHandler, C, D, Array<P>> =>
+  environment<SQSHandler, C, D, Array<P>>(config).payload(jsonSQSMessages).successHandler(response)
