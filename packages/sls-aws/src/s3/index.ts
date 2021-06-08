039035a00a931bb057570507094f559c04300be9
George Gunderson <georgegunderson@192-168-1-103.tpgi.com.au>
2021-06-08 19:26:33 +1000

    fix: remove unused s3Records handler

--- packages/sls-aws/src/s3/index.ts
+++ packages/sls-aws/src/s3/index.ts
@@ -2,7 +2,6 @@ import { EnvironmentConfig, Handler, SlsEnvironment, environment } from '@cabiri
 import type { Context, S3Event, S3EventRecord } from 'aws-lambda'
 import { response } from '../reponse/response-or-error'
 import { s3Record as _s3Record } from './s3-record'
-import { s3Records as _s3Records } from './s3-records'
 
 export type S3Record = S3EventRecord['s3']
 
@@ -12,8 +11,3 @@ export const s3Record = <D, C = never>(
   config?: EnvironmentConfig<S3Handler>
 ): SlsEnvironment<S3Handler, C, D, S3Record> =>
   environment<S3Handler, C, D, S3Record>(config).payload(_s3Record).successHandler(response)
-
-export const s3Records = <D, C = never>(
-  config?: EnvironmentConfig<S3Handler>
-): SlsEnvironment<S3Handler, C, D, Array<S3Record>> =>
-  environment<S3Handler, C, D, Array<S3Record>>(config).payload(_s3Records).successHandler(response)
