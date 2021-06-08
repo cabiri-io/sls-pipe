a679f94f09a84b410ca0b4e81e223635853a1019
George Gunderson <georgegunderson@192-168-1-103.tpgi.com.au>
2021-06-08 16:27:54 +1000

    feat: add eventbridge event and message handlers

--- packages/sls-aws/src/eventbridge/index.ts
+++ packages/sls-aws/src/eventbridge/index.ts
@@ -1,17 +1,18 @@
 import { EnvironmentConfig, Handler, SlsEnvironment, environment } from '@cabiri-io/sls-env'
 import type { Context, EventBridgeEvent } from 'aws-lambda'
 import { response } from '../reponse/response-or-error'
+import { jsonEventBridgeEvent } from './json-eventbridge-event'
+import { jsonEventBridgeMessage } from './json-eventbridge-message'
 
-export type EventBridgeHandler<T extends string, D> = Handler<EventBridgeEvent<T, D>, Context, Promise<void>>
+export type EventBridgeEventHandler = Handler<EventBridgeEvent<string, never>, Context, Promise<void>>
+export type EventBridgeMessageHandler = Handler<EventBridgeEvent<string, string>, Context, Promise<void>>
 
-const eventBridgeEvent = <T extends string, D>(
-  event: EventBridgeEvent<T, D>,
-  _context: Context
-): EventBridgeEvent<T, D> => event
+export const eventBridgeEvent = <D, P extends EventBridgeEvent<string, any>, C = never>(
+  config?: EnvironmentConfig<EventBridgeEventHandler>
+): SlsEnvironment<EventBridgeEventHandler, C, D, P> =>
+  environment<EventBridgeEventHandler, C, D, P>(config).payload(jsonEventBridgeEvent).successHandler(response)
 
-export const eventBridge = <D, DetailType extends string, Detail, C = never>(
-  config?: EnvironmentConfig<EventBridgeHandler<DetailType, Detail>>
-): SlsEnvironment<EventBridgeHandler<DetailType, Detail>, C, D, EventBridgeEvent<DetailType, Detail>> =>
-  environment<EventBridgeHandler<DetailType, Detail>, C, D, EventBridgeEvent<DetailType, Detail>>(config)
-    .payload(eventBridgeEvent)
-    .successHandler(response)
+export const eventBridgeMessage = <D, P, C = never>(
+  config?: EnvironmentConfig<EventBridgeMessageHandler>
+): SlsEnvironment<EventBridgeMessageHandler, C, D, P> =>
+  environment<EventBridgeMessageHandler, C, D, P>(config).payload(jsonEventBridgeMessage).successHandler(response)
