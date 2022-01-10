47efd0d28b211d4e7e81c1c9ef270295194bff55
George Gunderson <gundersong@users.noreply.github.com>
2022-01-10 22:17:40 +1000

    fix: eventBridgeMessage handler to pass details object (#52)
    * fix: eventBridgeMessage handler to pass details object
    
    * fix: json event bridge message tests
    
    * feat: add generic type to event bridge message handler
    
    Co-authored-by: George Gunderson <georgegunderson@192-168-1-105.tpgi.com.au>

--- packages/sls-aws/src/eventbridge/json-eventbridge-message.ts
+++ packages/sls-aws/src/eventbridge/json-eventbridge-message.ts
@@ -1,13 +1,5 @@
 import type { Context, EventBridgeEvent } from 'aws-lambda'
 
-import { PayloadParseError } from '../error/parse'
-
-const jsonEventBridgeMessage = <T>(event: EventBridgeEvent<string, string>, _context: Context): T => {
-  try {
-    return (JSON.parse(event.detail) as unknown) as T
-  } catch (err) {
-    throw new PayloadParseError('failed to process eventbridge message to json', { originalError: err })
-  }
-}
+const jsonEventBridgeMessage = <T>(event: EventBridgeEvent<string, T>, _context: Context): T => event.detail
 
 export { jsonEventBridgeMessage }
